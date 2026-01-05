daac34daeb250f4c47dd04e2da42712fdf49d974
ViniciusK-PC <viniciuskb36@gmail.com>
2026-01-05 04:05:40 -0300

    feat: updated Meus Pedidos icon to document/list icon

--- src/app/features/store/pages/store-landing/store-landing.component.ts
+++ src/app/features/store/pages/store-landing/store-landing.component.ts
@@ -52,8 +52,11 @@ import { RouterLink } from '@angular/router';
             </a>
             <a routerLink="/login" class="action-box">
               <svg class="action-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M3 18v-6a9 9 0 0 1 18 0v6"></path>
-                <path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path>
+                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                <polyline points="14 2 14 8 20 8"></polyline>
+                <line x1="16" y1="13" x2="8" y2="13"></line>
+                <line x1="16" y1="17" x2="8" y2="17"></line>
+                <polyline points="10 9 9 9 8 9"></polyline>
               </svg>
               <div class="action-label">
                 <span>Meus</span>
