10372e733ef4c61203cc40c2c2ebef29669c4620
ViniciusK-PC <viniciuskb36@gmail.com>
2026-01-07 23:45:59 -0300

    fix: Fix do Front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://back-end-production-1d27.up.railway.app/api',
+  apiUrl: 'https://back-end-production-e2a2.up.railway.app/api',
 };
 
